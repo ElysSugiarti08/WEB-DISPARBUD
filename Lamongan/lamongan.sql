515887b7760a7913e117ed5b8589aea2af2a17da
erna setia <ernasetia168@gmail.com>
2020-07-02 22:18:31 +0700

    tambahan tabel komentar by erna

--- Lamongan/lamongan.sql
+++ Lamongan/lamongan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Jul 2020 pada 08.05
--- Versi server: 10.1.36-MariaDB
--- Versi PHP: 7.2.11
+-- Generation Time: Jul 02, 2020 at 05:08 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jeniskelamin`
+-- Table structure for table `jeniskelamin`
 --
 
 CREATE TABLE `jeniskelamin` (
@@ -34,7 +34,7 @@ CREATE TABLE `jeniskelamin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `jeniskelamin`
+-- Dumping data for table `jeniskelamin`
 --
 
 INSERT INTO `jeniskelamin` (`id_jk`, `ket_jk`) VALUES
@@ -44,45 +44,52 @@ INSERT INTO `jeniskelamin` (`id_jk`, `ket_jk`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `komentar`
+-- Table structure for table `komentar`
 --
 
 CREATE TABLE `komentar` (
-  `id_komen` int(11) NOT NULL,
-  `isi_komen` varchar(100) NOT NULL,
-  `username` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_komen` int(50) NOT NULL,
+  `isi_komen` varchar(200) NOT NULL,
+  `email_komen` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `komentar`
+--
+
+INSERT INTO `komentar` (`id_komen`, `isi_komen`, `email_komen`) VALUES
+(6, 'halo, webnya bagus, semoga semakin uwu yaaa ^ ^', 'ernasetia168@gmail.com'),
+(7, 'haloooooo', 'ernaset@gmail.com'),
+(8, 'semoga nilai kita bagus ya guys, Aamiin', 'ernaset@gmail.com');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
-  `email` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `jenisKelamin` int(1) NOT NULL,
+  `jenisKelamin` int(11) NOT NULL,
   `noTelp` varchar(13) NOT NULL,
   `usertype` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id_user`, `username`, `email`, `password`, `jenisKelamin`, `noTelp`, `usertype`) VALUES
-(18, 'admin1', 'admin1@gmail.com', '$2y$10$bayiERR0i6QHrICnzIb5i.DUnX8JiRqUCN.O9N4etbRLsPVPoxcgO', 1, '08934383292', 1),
-(19, 'andini', 'arystayuni01@gmail', '$2y$10$ZNZVqTprn0Qw4uhomvjH2ewkdk9kuB2V1x8zswWlCMC.nGJmNFKkC', 2, '081289075634', 2),
-(20, 'arysta', 'andiniarysta@gmail.c', '$2y$10$yO4t3t8Pka4heXBDaLmPZu7eb7q.UArDB8aeTtywaQWJIjmtaCyaO', 2, '081289075634', 2),
-(21, 'afil', 'andiniarysta@gmail.c', '$2y$10$iHjKTZSX0stMV7agp0z7/OSMZzn2pJSTObEuOxnmmieRl/W5Mhq0O', 1, '081289075634', 2);
+(21, 'erna', 'ernasetia168@gmail.com', '$2y$10$A9jkj1A9sLRRPynBC4jgSuEvC3QfKuPR2sSiS0fFqenDc3T.S37nu', 2, '085807116666', 1),
+(25, 'ernana', 'ernaset@gmail.com', '$2y$10$esr.LDjyXrYUvjgYyN5LOO.8NH5DJFU5/0O3yNK4Xi6qSJpuo2hE6', 2, '085807116666', 2);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `usertype`
+-- Table structure for table `usertype`
 --
 
 CREATE TABLE `usertype` (
@@ -91,7 +98,7 @@ CREATE TABLE `usertype` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `usertype`
+-- Dumping data for table `usertype`
 --
 
 INSERT INTO `usertype` (`id_usertype`, `ket_usertype`) VALUES
@@ -103,19 +110,19 @@ INSERT INTO `usertype` (`id_usertype`, `ket_usertype`) VALUES
 --
 
 --
--- Indeks untuk tabel `jeniskelamin`
+-- Indexes for table `jeniskelamin`
 --
 ALTER TABLE `jeniskelamin`
   ADD PRIMARY KEY (`id_jk`);
 
 --
--- Indeks untuk tabel `komentar`
+-- Indexes for table `komentar`
 --
 ALTER TABLE `komentar`
   ADD PRIMARY KEY (`id_komen`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
@@ -123,33 +130,33 @@ ALTER TABLE `users`
   ADD KEY `fk_usertpe` (`usertype`);
 
 --
--- Indeks untuk tabel `usertype`
+-- Indexes for table `usertype`
 --
 ALTER TABLE `usertype`
   ADD PRIMARY KEY (`id_usertype`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `komentar`
+-- AUTO_INCREMENT for table `komentar`
 --
 ALTER TABLE `komentar`
-  MODIFY `id_komen` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_komen` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `users`
+-- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `fk_jenisKelamin` FOREIGN KEY (`jenisKelamin`) REFERENCES `jeniskelamin` (`id_jk`),
